07b7e859568d73e937e020920d7a9f499f30f882
root <jw8191@nyu.edu>
2026-02-27 06:55:41 +0000

    Milestone 8

--- nyush.c
+++ nyush.c
@@ -136,6 +136,124 @@ static int parse_redirections(char **argv, int *argc, int *redirect_in, char **i
     return 0;
 }
 
+static int is_builtin(const char *s)
+{
+    return s && (!strcmp(s, "cd") || !strcmp(s, "exit") || !strcmp(s, "jobs") || !strcmp(s, "fg"));
+}
+
+static int find_pipe(char **argv, int argc)
+{
+    for (int i = 0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "|") == 0)
+            return i;
+    }
+    return -1;
+}
+
+static void exec_with_path(char **argv)
+{
+    char *prog = resolve_program(argv[0]);
+    if (!prog)
+    {
+        fprintf(stderr, "Error: invalid program\n");
+        _exit(1);
+    }
+    execv(prog, argv);
+    _exit(1);
+}
+
+static void run_single_pipe(char **argvL, char **argvR, int redirect_in, char *infile,
+                            int redirect_out, int append, char *outfile)
+{
+    int pfd[2];
+    if (pipe(pfd) < 0)
+    {
+        fprintf(stderr, "Error: invalid program\n");
+        return;
+    }
+
+    pid_t pid1 = fork();
+    if (pid1 < 0)
+    {
+        fprintf(stderr, "Error: invalid program\n");
+        close(pfd[0]);
+        close(pfd[1]);
+        return;
+    }
+    if (pid1 == 0)
+    {
+        // child
+        child_restore_signals();
+
+        if (redirect_in)
+        {
+            int fd = open(infile, O_RDONLY);
+            if (fd < 0)
+            {
+                fprintf(stderr, "Error: invalid file\n");
+                _exit(1);
+            }
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+        }
+
+        dup2(pfd[1], STDOUT_FILENO);
+
+        close(pfd[0]);
+        close(pfd[1]);
+
+        exec_with_path(argvL);
+    }
+
+    pid_t pid2 = fork();
+    if (pid2 < 0)
+    {
+        fprintf(stderr, "Error: invalid program\n");
+        close(pfd[0]);
+        close(pfd[1]);
+        int st;
+        waitpid(pid1, &st, 0);
+        return;
+    }
+    if (pid2 == 0)
+    {
+        // child
+        child_restore_signals();
+
+        dup2(pfd[0], STDIN_FILENO);
+
+        if (redirect_out)
+        {
+            int fd;
+            if (append)
+                fd = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            else
+                fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0)
+            {
+                fprintf(stderr, "Error: invalid file\n");
+                _exit(1);
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+        }
+
+        close(pfd[0]);
+        close(pfd[1]);
+
+        exec_with_path(argvR);
+    }
+
+    // parent
+    close(pfd[0]);
+    close(pfd[1]);
+
+    int st;
+    waitpid(pid1, &st, 0);
+    waitpid(pid2, &st, 0);
+}
+
 int main(void)
 {
     shell_ignore_signals();
@@ -215,6 +333,76 @@ int main(void)
             break;
         }
 
+        int pipe_idx = find_pipe(argv, argc);
+
+        if (pipe_idx >= 0)
+        {
+            for (int j = pipe_idx + 1; j < argc; j++)
+            {
+                if (strcmp(argv[j], "|") == 0)
+                {
+                    fprintf(stderr, "Error: invalid command\n");
+                    free(argv);
+                    free(line_copy);
+                    continue;
+                }
+            }
+
+            // pipe cannot be head or tail
+            if (pipe_idx == 0 || pipe_idx == argc - 1)
+            {
+                fprintf(stderr, "Error: invalid command\n");
+                free(argv);
+                free(line_copy);
+                continue;
+            }
+
+            argv[pipe_idx] = NULL;
+            char **argvL = argv;
+            int argcL = pipe_idx;
+            char **argvR = &argv[pipe_idx + 1];
+            int argcR = argc - pipe_idx - 1;
+
+            // builtin cannot be piped
+            if (is_builtin(argvL[0]) || is_builtin(argvR[0]))
+            {
+                fprintf(stderr, "Error: invalid command\n");
+                free(argv);
+                free(line_copy);
+                continue;
+            }
+
+            // parse redirections
+            int Lin = 0, Lout = 0, Lapp = 0;
+            char *Linf = NULL, *Loutf = NULL;
+            int Rin = 0, Rout = 0, Rapp = 0;
+            char *Rinf = NULL, *Routf = NULL;
+
+            if (parse_redirections(argvL, &argcL, &Lin, &Linf, &Lout, &Lapp, &Loutf) < 0 ||
+                parse_redirections(argvR, &argcR, &Rin, &Rinf, &Rout, &Rapp, &Routf) < 0)
+            {
+                fprintf(stderr, "Error: invalid command\n");
+                free(argv);
+                free(line_copy);
+                continue;
+            }
+
+            if (Lout || Rin)
+            {
+                fprintf(stderr, "Error: invalid command\n");
+                free(argv);
+                free(line_copy);
+                continue;
+            }
+
+            run_single_pipe(argvL, argvR, Lin, Linf, Rout, Rapp, Routf);
+
+            free(argv);
+            free(line_copy);
+            continue;
+        }
+
+        // no pipe
         if (parse_redirections(argv, &argc, &redirect_in, &infile, &redirect_out, &append, &outfile) < 0)
         {
             fprintf(stderr, "Error: invalid command\n");
